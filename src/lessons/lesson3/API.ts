ad3abf9e9c009801f3aa62d87dc8539762b3827e
Nadyushka <12nadya11@gmail.com>
2023-01-19 21:28:44 +0300

    lesson 2 start

--- src/lessons/lesson3/API.ts
+++ src/lessons/lesson3/API.ts
@@ -8,6 +8,7 @@ const axiosInstance = axios.create(configOMB);
 
 const API = {
     searchFilmsByTitle: (title: string) => {
+        axios.get('https://www.omdbapi.com/?i=tt3896198&apikey=52eab7a1').then(res=>res.data.title)
     },
     searchFilmsByType: (title: string, type: string) => {
     }
